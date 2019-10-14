8ab3ae9f46713fdb43354596766a0f9ef8a4d312
Mersiha Ćeranić <mersihace@mistral.ba>
2019-10-14 16:15:12 +0200

    initial commit

---
+++ TimeKeeper.Domain/Assignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeKeeper.Domain
+{
+    public class Assignment : BaseClass
+    {
+        public int DayId { get; set; }
+        public int ProjectId { get; set; }
+        public string Description { get; set; }
+        public decimal Hours { get; set; }
+
+    }
+}
---
+++ TimeKeeper.Domain/Calendar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeKeeper.Domain
+{
+    public class Calendar
+    {
+        public Calendar()
+        {
+            Tasks = new List<Assignment>();
+        }
+        public int EmployeeId { get; set; }
+        public CalendarType CalendarType { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Hours { get; set; }
+        public virtual IList<Assignment> Tasks { get; set; }
+    }
+}
