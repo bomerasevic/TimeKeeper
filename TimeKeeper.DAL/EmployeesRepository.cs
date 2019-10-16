92df5f06fc31c9f35e1837d2b33a96f776580f62
Mersiha Ćeranić <mersihace@mistral.ba>
2019-10-16 11:22:06 +0200

    Added seed project

--- TimeKeeper.DAL/EmployeesRepository.cs
+++ TimeKeeper.DAL/EmployeesRepository.cs
@@ -10,5 +10,16 @@ namespace TimeKeeper.DAL
     public class EmployeesRepository : Repository<Employee>
     {
         public EmployeesRepository(TimeKeeperContext context) : base(context) { }
+
+        public override void Update(Employee employee, int id)
+        {
+            Employee old = Get(id);
+
+            if(old != null)
+            {
+                _context.Entry(old).CurrentValues.SetValues(employee);
+
+            }
+        }
     }
 }
