e7b4b881373c5cfe37eaf051f3cd5d311af11094
Berina <berina.omerasevic97@gmail.com>
2019-10-25 10:55:53 +0200

    Added Services section

--- TimeKepeer.Web/src/components/TeamMember/TeamMember.js
+++ TimeKepeer.Web/src/components/TeamMember/TeamMember.js
@@ -1,22 +1,20 @@
 import React from "react";
 import "./TeamMember.css";
-class TeamMember extends React.Component {
-    render() {
-        console.log(this.props.icon);
-        return (
-            <div class="team-member-card">
-                <div class="avatar">
-                    <img src={this.props.icon} alt="description"></img>
-                </div>
-                <h3>{this.props.name}</h3>
-                <h5>{this.props.role}</h5>
-                <img
-                    className="linkendin-icon"
-                    src="https://image.flaticon.com/icons/svg/174/174857.svg"
-                    alt="icon"
-                ></img>
+function TeamMember() {
+    console.log(this.props.icon);
+    return (
+        <div className="team-member-card">
+            <div className="avatar">
+                <img src={this.props.icon} alt="description"></img>
             </div>
-        );
-    }
+            <h3>{this.props.name}</h3>
+            <h5>{this.props.role}</h5>
+            <img
+                classNameName="linkendin-icon"
+                src="https://image.flaticon.com/icons/svg/174/174857.svg"
+                alt="icon"
+            ></img>
+        </div>
+    );
 }
 export default TeamMember;
