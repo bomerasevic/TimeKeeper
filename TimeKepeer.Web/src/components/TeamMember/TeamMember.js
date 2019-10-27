e7df8ad70723cf38d7aaefd0c772b8ff5f6d3b31
Hamza <hamza.crnovrsanin@gmail.com>
2019-10-27 21:45:09 +0100

    Styling some carousel components and icons

--- TimeKepeer.Web/src/components/TeamMember/TeamMember.js
+++ TimeKepeer.Web/src/components/TeamMember/TeamMember.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./TeamMember.css";
 import facebook from "../../assets/images/facebook.svg";
 import linkedin from "../../assets/images/linkedin.svg";
-import twitter from "../../assets/images/twitter.svg";
+import github from "../../assets/images/github.svg";
 class TeamMember extends React.Component {
     render() {
         console.log(this.props.icon);
         return (
             <div className="rectangle">
-                <img src={this.props.icon} alt="description"></img>
+                <img className="gray" src={this.props.icon} alt="description"></img>
                 <h4>{this.props.name}</h4>
                 <h6>{this.props.role}</h6>
 
                 <div className="row">
                     <div className="col s4 icon">
-                        <img src={twitter} />
+                        <img src={github} />
                     </div>
                     <div className="col s4 icon">
                         <img src={facebook} />
