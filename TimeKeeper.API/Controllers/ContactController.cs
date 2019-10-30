d33c6fd4e1e3bc60ab5e6b58585f88e01eaa4da9
Mersiha Ćeranić <mersihace@mistral.ba>
2019-10-30 16:02:03 +0100

    Added mail service

---
+++ TimeKeeper.API/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeKeeper.API.Models;
+using TimeKeeper.API.Services;
+
+namespace TimeKeeper.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase  // ovdje nam ne treba unitofwork
+    {
+        public IActionResult PostEmail([FromBody] MailModel mail)
+        {
+            try
+            {
+                string mailTo = mail.Email;
+                string subject = $"Contact request from {mail.Name}";
+                string body = $"{mail.Message}, {mail.Phone}";
+                MailService.Send(mailTo, subject, body);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }            
+        }
+    }
+}
--- TimeKeeper.Test/TestControllers/TestProjects.cs
+++ TimeKeeper.Test/TestControllers/TestProjects.cs
@@ -1,10 +1,117 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using TimeKeeper.API.Controllers;
+using TimeKeeper.API.Models;
+using TimeKeeper.DAL;
+using TimeKeeper.Domain;
 
 namespace TimeKeeper.Test.TestControllers
 {
-    class TestProjects
+    [TestFixture]
+    public class TestProjects
     {
+        public UnitOfWork unit;
+        public TimeKeeperContext context;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            string conStr = "User ID=postgres; Password=admin; Server=localhost; Port=5432; Database=testera; Integrated Security=true; Pooling=true;";
+            FileInfo fileLocation = new FileInfo(@"C:\TimeKeeper_DATA\TimeKeeperTest.xlsx");
+            context = new TimeKeeperContext(conStr);
+            unit = new UnitOfWork(context);
+            unit.Seed(fileLocation);
+        }
+
+        [Test, Order(1)]
+        public void GetAll()
+        {
+            var controller = new ProjectsController(context);
+            var response = controller.Get() as ObjectResult;
+            var value = response.Value as List<ProjectModel>;
+            Assert.AreEqual(200, response.StatusCode);
+            Assert.AreEqual(3, value.Count);
+        }
+        [TestCase(1), Order(2)]
+        public void GetById(int id)
+        {
+            var controller = new ProjectsController(context);
+            var response = controller.Get(id) as ObjectResult;
+            var value = response.Value as ProjectModel;
+            Assert.AreEqual(200, response.StatusCode);
+            Assert.AreEqual(1, value.Id);
+        }
+        [TestCase(11), Order(3)]
+        public void GetByWrongId(int id)
+        {
+            var controller = new ProjectsController(context);
+            var response = controller.Get(id) as ObjectResult;
+
+            Assert.Null(response);
+        }
+        [Test, Order(4)]
+        public void InsertProject()
+        {
+            var controller = new ProjectsController(context);
+            Project project = new Project
+            {
+                Name = "New role"
+            };
+            var response = controller.Post(project) as ObjectResult;
+            var value = response.Value as ProjectModel;
+
+            Assert.AreEqual(200, response.StatusCode);
+            Assert.AreEqual(6, value.Id);
+        }
+        [TestCase(1), Order(5)]
+        public void UpdateProject(int id)
+        {
+            var controller = new ProjectsController(context);
+
+            Project project = new Project
+            {
+                Id = id,
+                Name = "Updated!"
+            };
+
+            var response = controller.Put(id, project) as ObjectResult;
+            var value = response.Value as ProjectModel;
+
+            Assert.AreEqual(200, response.StatusCode);
+            Assert.AreEqual("Updated!", value.Name);
+        }
+        [TestCase(11), Order(6)]
+        public void UpdateProjectWithWrongId(int id)
+        {
+            var controller = new ProjectsController(context);
+
+            Project project = new Project
+            {
+                Id = id,
+                Name = "Updated!"
+            };
+            var response = controller.Put(id, project) as ObjectResult;
+            Assert.Null(response);
+        }
+        [TestCase(2), Order(7)]
+        public void DeleteProject(int id)
+        {
+            var controller = new ProjectsController(context);
+
+            var response = controller.Delete(id) as StatusCodeResult;
+            Assert.AreEqual(204, response.StatusCode);
+        }
+        [TestCase(11), Order(8)]
+        public void WrongDelete(int id)
+        {
+            var controller = new ProjectsController(context);
+
+            var response = controller.Delete(id) as StatusCodeResult;
+            Assert.AreEqual(404, response.StatusCode);
+        }
     }
 }
