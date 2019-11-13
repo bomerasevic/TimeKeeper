e4b6d29f8c65f43c3fa5770ff51b9289a1a816a5
mersiha ceranic <mersiha96@LIVE.COM>
2019-11-13 19:10:56 +0100

    Update of Projects Controller authorization

--- TimeKeeper.API/Controllers/CalendarController.cs
+++ TimeKeeper.API/Controllers/CalendarController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TimeKeeper.API.Factory;
+using TimeKeeper.API.Services;
 using TimeKeeper.DAL;
 using TimeKeeper.Domain;
 
@@ -15,7 +17,11 @@ namespace TimeKeeper.API.Controllers
     [ApiController]
     public class CalendarController : BaseController
     {
-        public CalendarController(TimeKeeperContext context) : base(context) { }
+        public TeamCalendarService teamCalendarService;
+        public CalendarController(TimeKeeperContext context) : base(context)
+        {
+            teamCalendarService = new TeamCalendarService(Unit);
+        } 
 
         /// <summary>
         /// This method returns all Days
@@ -63,19 +69,19 @@ namespace TimeKeeper.API.Controllers
                 return HandleException(ex);
             }
         }
-
-        //[HttpGet("team-time-tracking/{teamId}/{year}/{month}")]
-        //public IActionResult GetTimeTracking(int teamId, int year, int month)
-        //{
-        //    try
-        //    {
-        //        return Ok(teamCalendarService.TeamMonthReport(teamId, month, year));
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return HandleException(ex);
-        //    }
-        //}
+        [AllowAnonymous]
+        [HttpGet("team-time-tracking/{teamId}/{year}/{month}")]
+        public IActionResult GetTimeTracking(int teamId, int year, int month)
+        {
+            try
+            {
+                return Ok(teamCalendarService.TeamMonthReport(teamId, month, year));
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
         /// <summary>
         /// Inserts new Day
         /// </summary>
